9588f7ea9fe0dd076feee6e042f8dda09305b281
Cameron <quiksilver980@gmail.com>
2018-04-24 14:20:51 -0700

    New and Improved DDL!

--- StoreFrontDDL.sql
+++ StoreFrontDDL.sql
@@ -12,40 +12,77 @@ CREATE TABLE Users
   UserId INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   UserName VARCHAR(30) NOT NULL,
   Password VARCHAR(30) NOT NULL,
-  Address VARCHAR(300),
-  isStoreOwner BOOLEAN NOT NULL,
-  StoreName VARCHAR(50),
+  StoreName VARCHAR(50) NOT NULL,
 
   UNIQUE (UserName, Password)
 );
 
+INSERT INTO
+	Users (UserName, Password, StoreName)
+VALUES
+	('StoreOwner1','test1', '1'),
+	('StoreOwner2','test2', '2'),
+	('StoreOwner3','test3', '3'),
+	('StoreOwner4','test4', '3');
+
 CREATE TABLE Products
 (
   ProductId INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  UserId INTEGER UNSIGNED NOT NULL,
   ProductName VARCHAR(50) NOT NULL,
   Description VARCHAR(300),
   Price FLOAT(13,2) NOT NULL,
   SKU VARCHAR(20),
   Count INTEGER SIGNED NOT NULL,
-  isActive BOOLEAN NOT NULL
+  isActive BOOLEAN NOT NULL,
+
+  FOREIGN KEY (UserId) REFERENCES Users(UserId) ON DELETE CASCADE
 );
+
+INSERT INTO
+	Products (UserId, ProductName, Description, Price, SKU, Count, isActive)
+VALUES
+	(1, 'Pencil1', 'Description1', 11.11, '1234567890', 30, 1),
+	(1, 'Pen1', 'Description11', 11.11, '1234567890', 30, 1),
+	(2, 'Pencil2', 'Description2', 22.22, '1234567890', 30, 1),
+	(2, 'Pen2', 'Description22', 22.22, '1234567890', 30, 1),
+	(3, 'Pencil2', 'Description3', 33.33, '1234567890', 30, 1),
+	(3, 'Pen3', 'Description33', 33.33, '1234567890', 30, 1),
+	(4, 'Pencil3', 'Description44', 44.44, '1234567890', 30, 1),
+	(4, 'Pen4', 'Description44', 44.44, '1234567890', 30, 1);
 
 CREATE TABLE Orders
 (
   OrderId INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  StoreOwnerId INTEGER UNSIGNED NOT NULL,
-  ProductId INTEGER UNSIGNED NOT NULL,
-  Status VARCHAR(20) NOT NULL DEFAULT 'Ordered',
-
-  FOREIGN KEY (StoreOwnerId) REFERENCES Users(UserId) ON DELETE CASCADE,
-  FOREIGN Key (ProductId) REFERENCES Products(ProductId) ON DELETE CASCADE
-);
-
-CREATE TABLE Inventory
-(
   UserId INTEGER UNSIGNED NOT NULL,
   ProductId INTEGER UNSIGNED NOT NULL,
+  Status VARCHAR(20) NOT NULL DEFAULT 'Ordered',
+  FirstName VARCHAR(30) NOT NULL,
+  LastName VARCHAR(30) NOT NULL,
+  Address VARCHAR(300) NOT NULL,
 
   FOREIGN KEY (UserId) REFERENCES Users(UserId) ON DELETE CASCADE,
   FOREIGN Key (ProductId) REFERENCES Products(ProductId) ON DELETE CASCADE
 );
+
+INSERT INTO
+  Orders (UserId, ProductId, FirstName, LastName, Address)
+VALUES
+  (1, 1, 'First1', 'Last1', '1 test cir, test town, CA 12345'),
+  (1, 2, 'First1', 'Last1', '1 test cir, test town, CA 12345'),
+  (2, 3, 'First2', 'Last2', '2 test cir, test town, CA 12345'),
+  (2, 4, 'First2', 'Last2', '2 test cir, test town, CA 12345'),
+  (3, 5, 'First3', 'Last3', '3 test cir, test town, CA 12345'),
+  (3, 6, 'First3', 'Last3', '3 test cir, test town, CA 12345'),
+  (4, 7, 'First4', 'Last4', '4 test cir, test town, CA 12345'),
+  (4, 8, 'First4', 'Last4', '4 test cir, test town, CA 12345');
+
+
+-- CREATE TABLE Inventory
+-- (
+--   UserId INTEGER UNSIGNED NOT NULL,
+--   ProductId INTEGER UNSIGNED NOT NULL,
+--
+--   FOREIGN KEY (UserId) REFERENCES Users(UserId) ON DELETE CASCADE,
+--   FOREIGN Key (ProductId) REFERENCES Products(ProductId) ON DELETE CASCADE
+-- );
